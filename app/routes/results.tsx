aac07fc42b1f6fd697bf8a8c943daa92bc4f117a
owen-merry <owen.merry@skyscanner.net>
2022-10-07 16:16:48 +0100

    used context for env info

--- app/routes/results.tsx
+++ app/routes/results.tsx
@@ -2,24 +2,27 @@ import type { LoaderFunction } from "@remix-run/cloudflare";
 import { useLoaderData } from "@remix-run/react";
 import { json } from "@remix-run/cloudflare";
 
-export const loader: LoaderFunction = async ({ request }) => {
+export const loader: LoaderFunction = async ({ request, context }) => {
   const url = new URL(request.url);
   const term = url.searchParams.get("term");
 
   const res = await fetch(
-    `https://api.giphy.com/v1/gifs/search?api_key=${process.env.GIPHY_API_KEY}&q=${term}&limit=25&offset=0&rating=g&lang=en`
+    `https://api.giphy.com/v1/gifs/search?api_key=${context.GIPHY_API_KEY}&q=${term}&limit=25&offset=0&rating=g&lang=en`
   );
   const gifs = await res.json();
+  console.log("get gifs...", context.GIPHY_API_KEY);
 
   return json({
     term,
     gifs,
+    gifkey: context.GIPHY_API_KEY,
   });
 };
 
 export default function ResultsPage() {
-  const { term, gifs } = useLoaderData<{
+  const { term, gifs, gifkey } = useLoaderData<{
     term: string;
+    gifkey: string;
     gifs: {
       data: {
         type: string;
@@ -31,7 +34,9 @@ export default function ResultsPage() {
 
   return (
     <div>
-      <h1>Results ({term})</h1>
+      <h1>
+        Results ({term}) ({gifkey})
+      </h1>
       {gifs.data.map((gif) => (
         <span key={gif.id}>
           <iframe src={gif.embed_url} title="gif"></iframe>
